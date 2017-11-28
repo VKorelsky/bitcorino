187561c8b4d3e46c4afbe8cefba288d64265b027
Victor Korelsky <victor.korelsky@mail.mcgill.ca>
2017-11-28 13:44:56 -0500

    dumb bot v1

---
+++ index.js
@@ -0,0 +1,46 @@
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+const axios = require('axios')
+
+// load keys
+require('dotenv').config()
+
+app.use(bodyParser.json()); // for parsing application/json
+app.use(bodyParser.urlencoded({
+  extended: true
+})); // for parsing application/x-www-form-urlencoded
+
+const TEL_KEY = process.env.TEL_KEY
+const SEND_MESSAGE_URL = 'https://api.telegram.org/bot${TEL_KEY}/sendMessage'
+
+//This is the route the API will call
+app.post('/scam', function(req, res) {
+  // message object has text and chat fields. chat has an ID, which is the id of the chat we are in
+  const {message} = req.body
+
+  // if no message or no matteo, reply nothing
+  if (!message || message.text.toLowerCase().indexOf('matteo') < 0) {
+    return res.end()
+  }
+
+  // if matteo, send a reply
+  axios.post(SEND_MESSAGE_URL, {
+    chat_id: message.chat.id,
+    text: 'SCAM!!'
+  })
+    .then(response => {
+      console.log('Message posted')
+      res.end('ok')
+    })
+    .catch(err => {
+      console.log('Error :', err)
+      res.end('Error :' + err)
+    })
+
+});
+
+// Finally, start our server
+app.listen(3000, function() {
+  console.log('Telegram app listening on port 3000!');
+});
